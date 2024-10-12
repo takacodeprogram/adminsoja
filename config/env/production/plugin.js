81eac9dcd63b4f2f7a4201d01920a4a6b28a92aa
takacodeprogram <takacodeprogram@gmail.com>
2024-10-12 20:57:52 +0200

    Update plugin.js

--- config/env/production/plugin.js
+++ config/env/production/plugin.js
@@ -12,18 +12,21 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+        localServer: {
+          maxage: 300000
+        },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
       },
     },
   },
   // ...
 });
+
