da35f7f57055ee0d74ecd69d83de206266a429b2
takacodeprogram <takacodeprogram@gmail.com>
2024-10-12 21:34:47 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 1000000, // Limite de taille en bytes (facultatif)
+      },
+      actionOptions: {
+        upload: {
+          folder: './public/upload', // Chemin personnalisé pour les fichiers
+        },
+        delete: {},
+      },
+    },
+  },
+});
